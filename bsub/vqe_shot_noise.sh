5d8c6a56c411312fc1f6e1d9a54b4718e81efea4
Flemming Morsch <flemming@cherimoyaanalytics.com>
2026-05-28 19:04:55 +0200

    vqe shot noise experiment

---
+++ bsub/vqe_shot_noise.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+### queue name
+#BSUB -q hpc
+### job name
+#BSUB -J vqe_shot_noise
+### number of cores
+#BSUB -n 24
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+### -- specify that we need 4GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=8GB]"
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 48:00
+### Output file and error file
+#BSUB -o vqe_shot_noise.out
+#BSUB -e vqe_shot_noise.err
+
+
+# Get used CPU
+lscpu
+
+
+## H2O
+~/micromamba/envs/qrunch/bin/python compare_vqe_shot_noise.py H2O 10 7 40 -b STO-3G -r 10 -o compare_vqe_methods
+
