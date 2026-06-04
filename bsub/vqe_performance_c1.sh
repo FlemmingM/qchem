951d00836d7958a2b8a39350fb9ebd2d353551c2
Flemming Morsch <flemming@cherimoyaanalytics.com>
2026-06-04 12:58:00 +0200

    use 1-6 cores jobs

---
+++ bsub/vqe_performance_c1.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+### queue name
+#BSUB -q hpc
+### job name
+#BSUB -J vqe_performance
+### number of cores
+#BSUB -n 1
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+### -- specify that we need 4GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=10GB]"
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 24:00
+### Output file and error file
+#BSUB -o compare_vqe_methods_c1.out
+#BSUB -e compare_vqe_methods_c1.err
+
+
+# Get used CPU
+lscpu
+
+## H2O
+~/micromamba/envs/qrunch/bin/python compare_vqe_methods_simple.py H2O 4 4 20 -b STO-3G -o compare_vqe_methods_c1
