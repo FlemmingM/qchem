f91dadf858f618ac9927b0324ad5d2a09bb4318f
Flemming Morsch <flemming@cherimoyaanalytics.com>
2026-06-07 22:38:19 +0200

    h2o study again

---
+++ bsub/vqe_performance_h2o_2.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+### queue name
+#BSUB -q hpc
+### job name
+#BSUB -J vqe_performance_h2o_2
+### number of cores
+#BSUB -n 1
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+### -- specify that we need 4GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=10GB]"
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 72:00
+### Output file and error file
+#BSUB -o vqe_performance_h2o_2.out
+#BSUB -e vqe_performance_h2o_2.err
+
+
+# Get used CPU
+lscpu
+
+# Run each molecule: minimal and Full CI
+
+## H2O
+# ~/micromamba/envs/qrunch/bin/python compare_vqe_methods.py H2O 4 4 20 -b cc-pvdz -o compare_vqe_methods
+
+# ~/micromamba/envs/qrunch/bin/python compare_vqe_methods.py H2O 6 5 40 -b cc-pvdz -o compare_vqe_methods
+# ~/micromamba/envs/qrunch/bin/python compare_vqe_methods.py H2O 8 6 40 -b cc-pvdz -o compare_vqe_methods
+# ~/micromamba/envs/qrunch/bin/python compare_vqe_methods.py H2O 10 7 40 -b cc-pvdz -o compare_vqe_methods
+
+## H2O
+~/micromamba/envs/qrunch/bin/python compare_vqe_methods.py H2O 2 2 3 -b STO-3G -o compare_vqe_methods_h2o_2
+~/micromamba/envs/qrunch/bin/python compare_vqe_methods.py H2O 4 4 20 -b STO-3G -o compare_vqe_methods_h2o_2
+~/micromamba/envs/qrunch/bin/python compare_vqe_methods.py H2O 6 5 40 -b STO-3G -o compare_vqe_methods_h2o_2
+~/micromamba/envs/qrunch/bin/python compare_vqe_methods.py H2O 8 6 40 -b STO-3G -o compare_vqe_methods_h2o_2
+~/micromamba/envs/qrunch/bin/python compare_vqe_methods.py H2O 10 7 40 -b STO-3G -o compare_vqe_methods_h2o_2
